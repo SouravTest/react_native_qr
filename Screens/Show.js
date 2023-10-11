6ea6755a373a9522f37fc8f814a745e9e32adede
SOURAV <MAIL.MYMAIL11@GMAIL.COM>
2023-10-11 13:02:57 +0530

    show qr design change

--- Screens/Show.js
+++ Screens/Show.js
@@ -1,10 +1,9 @@
-import { StyleSheet, Text, View, TouchableOpacity, Alert} from "react-native";
-import React, { useRef ,useState,useEffect} from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from "react-native";
+import React, { useRef, useState, useEffect } from "react";
 import QRCode from "react-native-qrcode-svg";
 import ViewShot from "react-native-view-shot";
-import * as MediaLibrary from 'expo-media-library';
-import { shareAsync } from 'expo-sharing';
-
+import * as MediaLibrary from "expo-media-library";
+import { shareAsync } from "expo-sharing";
 
 const Show = () => {
   let logoFromFile = require("../assets/adaptive-icon.png");
@@ -14,7 +13,8 @@ const Show = () => {
   useEffect(() => {
     (async () => {
       // const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
       // setHasCameraPermission(cameraPermission.status === "granted");
       setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
@@ -26,35 +26,46 @@ const Show = () => {
       if (result) {
         MediaLibrary.saveToLibraryAsync(result).then(() => {
           //console.log("QR Code saved to the media library.");
-          Alert.alert('Downloade','QR Code saved to the media library')
+          Alert.alert("Downloade", "QR Code saved to the media library");
         });
       }
     } catch (error) {
-      Alert.alert('Error','Error while saving');
+      Alert.alert("Error", "Error while saving");
       //console.error("Error saving QR Code:", error);
     }
-
   };
 
-
-  const handleShare = async()=>{
+  const handleShare = async () => {
     const result = await viewShotRef.current.capture();
     shareAsync(result).then(() => {
       // setPhoto(undefined);
     });
-  }
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Qr</Text>
-      <ViewShot  ref={viewShotRef} options={{ format: "png", quality: 1.0 }}>
-        <QRCode
-          value="https://play.google.com/store/apps/details?id=com.o2Academia.souravtech&pli=1"
-          logo={logoFromFile}
-          logoSize={20}
-        />
-          <Text style={{alignItems:"center",alignSelf:"center",color:'blue',fontSize:5}}>All about qr</Text>
-</ViewShot>
+      <ViewShot ref={viewShotRef} options={{ format: "png", quality: 1.0 }}>
+        <View style={{ padding: 20, backgroundColor: "white" }}>
+          <QRCode
+            value="https://play.google.com/store/apps/details?id=com.o2Academia.souravtech&pli=1"
+            logo={logoFromFile}
+            logoSize={20}
+            containerStyle={20}
+            size={200}
+          />
+          <Text
+            style={{
+              alignItems: "center",
+              alignSelf: "center",
+              color: "blue",
+              fontSize: 5,
+            }}
+          >
+            All about qr
+          </Text>
+        </View>
+      </ViewShot>
       <TouchableOpacity style={styles.button} onPress={handleDownload}>
         <Text style={styles.buttonText}>Download QR Code</Text>
       </TouchableOpacity>
@@ -84,15 +95,15 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   button: {
-    backgroundColor: 'black',
+    backgroundColor: "black",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
     marginTop: 10,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
+    color: "white",
+    fontWeight: "bold",
     fontSize: 16,
   },
 });
